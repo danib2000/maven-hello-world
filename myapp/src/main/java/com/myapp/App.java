2658830571875f6e765fab175624976d7ef1adf6
Daniel Borisenko <danib564@gmail.com>
2026-09-07 16:45:42 +0300

    Added a thread so app kept running

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! From Dani B" );
+
+	// Keep the application process running 
+	Thread.currentThread().join(); 
     }
 }
